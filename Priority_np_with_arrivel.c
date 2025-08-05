4afe94f0564e447d301cb71be0257b42f9e7e8ae
Mahi-rejone <mahirejone@gmail.com>
2025-08-05 19:45:32 +0600

    add priority code

--- Priority_np_with_arrivel.c
+++ Priority_np_with_arrivel.c
@@ -1,62 +1,63 @@
 #include <stdio.h>
+#define MAX 100
 
-void priority(int pid[], int n, int At[], int Bt[], int pr[]) {
-    int Ct[n], Wt[n], TAT[n];
-    float totalCt = 0, totalWt = 0, totalTat = 0;
+void priorityScheduling(int n, int pid[], int At[], int Bt[], int Pr[]) {
+    int Ct[MAX], Wt[MAX], TAT[MAX], completed[MAX]={0};
+    int current = 0, completedCount = 0;
+    float totalWt = 0, totalTat = 0, totalCt=0 ;
 
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-i-1; j++) {
-            if(At[j] > At[j+1]) {
-                int temp = pr[j];
-                pr[j] = pr[j+1];
-                pr[j+1] = temp;
+    printf("\nProcess Arrival Burst Priority Completion Waiting Turnaround\n");
 
-                int temp = At[j];
-                At[j] = At[j+1];
-                At[j+1] = temp;
+    while(completedCount < n){
+        int idx = -1;
+        int bestPriority = 9999;
 
-                temp = Bt[j];
-                Bt[j] = Bt[j+1];
-                Bt[j+1] = temp;
-
-                temp = pid[j];
-                pid[j] = pid[j+1];
-                pid[j+1] = temp;
+        for(int i = 0; i < n; i++){
+            if(At[i] <= current && !completed[i]){
+                if(Pr[i] < bestPriority){
+                    bestPriority = Pr[i];
+                    idx = i;
+                }
             }
         }
-    }
-    Ct[0] = Bt[0];
-    for (int i = 1; i < n; i++) {
-        Ct[i] = Ct[i-1] + Bt[i];
-    }
-    for(int i = 0; i < n; i++){
-        TAT[i] = Ct[i];
-        Wt[i] = Ct[i] - Bt[i];
 
-        totalCt  += Ct[i];
-        totalTat += TAT[i];
-        totalWt  += Wt[i];
+        if(idx == -1){
+            current++;
+        }
+        else{
+            current += Bt[idx];
+            Ct[idx] = current;
+            TAT[idx] = Ct[idx] - At[idx];
+            Wt[idx] = TAT[idx] - Bt[idx];
+            completed[idx] = 1;
+            completedCount++;
+
+            printf("P%d\t%d\t%d\t%d\t%d\t  %d\t  %d\n",
+                   pid[idx], At[idx], Bt[idx], Pr[idx], Ct[idx], Wt[idx], TAT[idx]);
+
+            totalWt += Wt[idx];
+            totalTat += TAT[idx];
+            totalCt += Ct[idx];
+        }
     }
-    printf("Process\tPriority\tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("p%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",pid[i],pr[i],Bt[i],Ct[i],Wt[i],TAT[i]);
-    }
-    printf("\nAverage Completion Time = %.2f(ms)\n", totalCt/n);
-    printf("Average Waiting Time    = %.2f(ms)\n", totalWt/n);
-    printf("Average Turnaround Time = %.2f(ms)\n", totalTat/n);
+
+    printf("\nAverage Waiting Time    = %.2f\n", totalWt/n);
+    printf("Average Turnaround Time = %.2f\n", totalTat/n);
+    printf("Average Complition Time = %.2f\n", totalCt/n);
 }
-int main() {
+
+int main(){
     int n;
     printf("Enter number of processes: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    int pid[n], At[n], Bt[n], Pr[n];
-
+    int pid[MAX], At[MAX], Bt[MAX], Pr[MAX];
     for(int i = 0; i < n; i++){
         pid[i] = i+1;
-        printf("Enter Arrivel Time, Burst Time, Priority for P%d: ", i+1);
-        scanf("%d,%d,%d",&At[i],&Bt[i],&Pr[i]);
+        printf("Enter Arrival Time, Burst Time and Priority for P%d: ", pid[i]);
+        scanf("%d %d %d", &At[i], &Bt[i], &Pr[i]);
     }
-    priority(pid,n,At,Bt,Pr);
+
+    priorityScheduling(n, pid, At, Bt, Pr);
     return 0;
 }
