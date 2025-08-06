d349046361ab198f0c58bc383171a63bd3fd7cce
Mahi-rejone <mahirejone@gmail.com>
2025-08-06 23:42:43 +0600

    add sjf fix coode

--- SJF_With_Arrivel_Time.c
+++ SJF_With_Arrivel_Time.c
@@ -1,66 +1,88 @@
 #include <stdio.h>
 
-void SJF(int pid[], int n, int At[], int Bt[]) {
-    int Ct[n], Wt[n], TAT[n], completed[n];
+void priority(int pid[], int n, int At[] ,int Bt[]) {
+    int Ct[n], Wt[n], TAT[n];
     float totalCt = 0, totalWt = 0, totalTat = 0;
-    int current = 0, completedCount = 0;
 
-    for(int i = 0; i < n; i++) {
-        completed[i] = 0;
-    }
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(At[j] > At[j+1]) {
 
-    printf("\nProcess\tAT\tBT\tCT\tWT\tTAT\n");
+                int temp = At[j];
+                At[j] = At[j+1];
+                At[j+1] = temp;
 
-    while(completedCount < n){
-        int idx = -1;
-        int minBT = 9999;
+                temp = Bt[j];
+                Bt[j] = Bt[j+1];
+                Bt[j+1] = temp;
 
-        for(int i = 0; i < n; i++){
-            if(At[i] <= current && !completed[i]){
-                if(Bt[i] < minBT){
-                    minBT = Bt[i];
-                    idx = i;
-                }
+                temp = pid[j];
+                pid[j] = pid[j+1];
+                pid[j+1] = temp;
             }
         }
+    }
+    for(int i = 1; i < n-1; i++) {
+        for(int j = 1; j < n-i; j++) {
+            if(Bt[j] > Bt[j+1]|| Bt[j]==Bt[j+1]&&At[j]>At[j+1] ) {
+                int temp = At[j];
+                At[j] = At[j+1];
+                At[j+1] = temp;
 
-        if(idx == -1){
-            current++; 
+                temp = Bt[j];
+                Bt[j] = Bt[j+1];
+                Bt[j+1] = temp;
+
+                temp = pid[j];
+                pid[j] = pid[j+1];
+                pid[j+1] = temp;
+            }
         }
-        else{
-            current += Bt[idx];
-            Ct[idx] = current;
-            TAT[idx] = Ct[idx] - At[idx];
-            Wt[idx] = TAT[idx] - Bt[idx];
-            completed[idx] = 1;
-            completedCount++;
-
-            totalCt  += Ct[idx];
-            totalTat += TAT[idx];
-            totalWt  += Wt[idx];
-
-            printf("P%d\t%d\t%d\t%d\t%d\t%d\n", pid[idx], At[idx], Bt[idx], Ct[idx], Wt[idx], TAT[idx]);
+    }
+    
+    Ct[0] = At[0] + Bt[0];
+    for(int i = 1; i < n; i++) {
+        if(Ct[i-1] < At[i]) {
+            Ct[i] = At[i] + Bt[i];
+        } else {
+            Ct[i] = Ct[i-1] + Bt[i];
         }
     }
 
-    printf("\nAverage Completion Time = %.2f\n", totalCt/n);
-    printf("Average Waiting Time    = %.2f\n", totalWt/n);
-    printf("Average Turnaround Time = %.2f\n", totalTat/n);
+    for(int i = 0; i < n; i++){
+        TAT[i] = Ct[i] - At[i];
+        Wt[i]  = TAT[i] - Bt[i];
+
+        totalCt  += Ct[i];
+        totalTat += TAT[i];
+        totalWt  += Wt[i];
+    }
+
+    printf("Process\tA.T\tB.T\tC.T\tW.T\tT.A.T\n");
+    for (int i = 0; i < n; i++) {
+        printf("p%d\t%d\t%d\t%d\t%d\t%d\n",pid[i],At[i],Bt[i],Ct[i],Wt[i],TAT[i]);
+    }
+
+    printf("\nAverage Completion Time = %.2f(ms)\n", totalCt/n);
+    printf("Average Waiting Time    = %.2f(ms)\n", totalWt/n);
+    printf("Average Turnaround Time = %.2f(ms)\n", totalTat/n);
 }
 
 int main() {
     int n;
     printf("Enter number of processes: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
 
     int pid[n], At[n], Bt[n];
+
+
     for(int i = 0; i < n; i++){
-        pid[i] = i+1;
-        printf("Enter Arrival time & Burst time for P%d: ", pid[i]);
-        scanf("%d %d", &At[i], &Bt[i]);
+        pid[i]=i+1;
+        printf("Enter arrivel time & burst time for P%d: ", i+1);
+        scanf("%d %d",&At[i],&Bt[i]);
     }
 
-    SJF(pid, n, At, Bt);
+    priority(pid,n,At,Bt);
 
     return 0;
 }
